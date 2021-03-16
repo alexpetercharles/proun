628d15ca2d089138cfcac578171fc51ed6ab7d6e
alexpetercharles <hi@alexpetercharles.ch>
2021-03-16 14:42:50 +0100

    removed p5

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,68 @@
 <script lang="ts">
 /* eslint-disable no-new */
-import { defineComponent, onMounted, ref } from 'vue';
-import P5 from 'p5';
-
-import proun from '@/sketches/proun';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
   setup: () => {
     const sketchContainer = ref({} as HTMLElement);
-    onMounted(() => new P5(proun(sketchContainer.value.offsetHeight,
-      sketchContainer.value.offsetWidth), sketchContainer.value));
+    const selectImage = (event: InputEvent) => {
+      const { files } = event.target as HTMLInputElement;
+      if (files) {
+        const reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+      }
+    };
     return {
       sketchContainer,
+      selectImage,
     };
   },
 });
 </script>
 
 <template>
-  <div class="sketch-container" ref="sketchContainer" />
+  <div class="sketch-container" ref="sketchContainer">
+    <div class="controls">
+      <label for="image">background image:</label>
+      <input id="image" type="file" accept="image/*" @change="selectImage" />
+      <label for="alpha">image opacity:</label>
+      <input id="alpha" type="range" min="0" max="255" v-model="prounState.imageAlpha"/>
+    </div>
+  </div>
+  <div class="blur-test" />
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 
   .sketch-container {
     height: 100vh;
     width: 100vw;
+  }
+
+  .controls {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  .blur-test {
+    height: 10vh;
+    width: 10vh;
+    backdrop-filter: blur(100px);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 999;
   }
 }
 </style>
